72c966f49f4834ab7586d18197ee58e4c7936d36
Nixon <a0124832@u.nus.edu>
2016-10-26 20:15:34 +0800

    Change the launcher

--- app/src/main/java/sg/edu/nus/ticketingstaffapp/Login.java
+++ app/src/main/java/sg/edu/nus/ticketingstaffapp/Login.java
@@ -1,7 +1,9 @@
 package sg.edu.nus.ticketingstaffapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClick_Login(View view){
+        Intent myIntent = new Intent(this,MainActivity.class);
+        startActivity(myIntent);
     }
 }
